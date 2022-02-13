0ff77a889b5f50bfe3acdf0ffd2740b27f133857
joaovitor644 <joaorasp666@yahoo.com>
2022-02-12 21:38:17 -0300

    realocação de arquivos e mudança no arquivo server.ts

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path'
 import consolreload from './tools/consoleUpdate';
 import Logger from "./tools/Logger"
 import { mainControler , Data } from './controllers/main'
@@ -10,7 +11,7 @@ const port = 3000
 
 app.set('views','app/views')
 app.set("view engine", "ejs")
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use('/', mainControler.index);
 app.use('/404', mainControler.status404);
